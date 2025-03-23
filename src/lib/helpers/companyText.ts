acfbbcf2df1a07cec9af9bcd05e55a540e3182d7
user2022 <marleydabb55@gmail.com>
2025-03-23 23:48:15 +0000

    Added in ethos section page

--- src/lib/helpers/companyText.ts
+++ src/lib/helpers/companyText.ts
@@ -4,4 +4,8 @@ export const CompanyText = {
     p1: 'Coming from a lifelong background in the double glazing industry, we began trading online in 2008. Our core product offering was composite doors, and we were one of the first companies in the UK to pioneer online ordering for this product.',
     p2: 'Over the past 15 years, we have expanded our portfolio of products across a range of composite, aluminium, uPVC and security products. Our goal was always to offer a complete home improvement solution, which we now carry out nationwide through GFD Homes.',
   },
+  ethos: {
+    p1: 'We know that our continued success is down to our customers, and treating them with respect. Starting from humble beginnings means that we value each and every customer equally, regardless of the order size.',
+    p2: 'As the world becomes more automated by the day, we promise never to lose the personal touch with our customers that we built the foundations of the business on. Our staff are friendly, knowledgeable and always happy to assist.',
+  },
 }
